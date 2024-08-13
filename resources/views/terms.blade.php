fb7a417996026ef45d12d79971065f9f10a941af
M. O. B. JIHAD <mobjihad15@gmail.com>
2024-08-13 19:17:20 +0600

    Nav bar and basic template

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+@section('content')
+    <div>
+        <h1>terms</h1>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quibusdam maiores totam optio magnam eius
+            debitis eveniet veniam illum voluptatibus? Impedit similique repellendus atque adipisci quod, alias modi quas
+            neque.
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/dashboard',[DashboardController::class,'index']);
 Route::get('/profile',[ProfileController::class,"index"]);
 Route::get('/registration',[registerController::class,'index']);
+Route::get('/terms',function(){
+    
+    return view('terms');
+
+});
